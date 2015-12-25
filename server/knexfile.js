f35db770441adfe739985bfe72f69f9f6056561c
HuarenYU <yuhuarenproject@gmail.com>
2015-12-25 23:50:07 +0800

    Add knex support

---
+++ server/knexfile.js
@@ -0,0 +1,53 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      database: 'tour_dev',
+      user:     'root',
+      password: 'yuhuaren'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'mysql',
+    connection: {
+      database: 'tour_staging',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      database: 'tour_production',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
